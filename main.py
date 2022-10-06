2938686d862c5023d28cd0e35b15bedf0bc79c9f
lungsang-bod <lungsang@esukhia.org>
2022-10-06 16:35:49 +0530

    credentials.json

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import streamlit as st
-from google.oauth2 import service_account
+from pydrive.auth import GoogleAuth
 
 
 def authenticate():
-    SCOPES = ['https://www.googleapis.com/auth/sqlservice.admin']
-    SERVICE_ACCOUNT_FILE = 'lustrous-bond-364709-c757fbc3c905.json'
-
-    credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
 
 
 st.button("click here", on_click=authenticate)
