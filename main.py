2046befbb229ed20d9f2ef01d8ec0cb51ab60bec
lungsang-bod <lungsang@esukhia.org>
2022-10-06 16:43:53 +0530

    gdrive path

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import streamlit as st
 from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 
 def authenticate():
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+    path = "/home/ubuntu/serverside-gdrive-authenticate/content/"
+    folder = "KrW8LGuIELpsBqvaVfLAmHiuLz7uJ5T_"
+
+    file_list = drive.ListFile({'q': f"'{folder}' in parents and trashed=false"}).GetList()
+    if st.checkbox("Show file list?", True):
+        for index, file in enumerate(file_list):
+            st.write(index + 1, file['title'])
+            file.GetContentFile(path + file['title'])
 
 
 st.button("click here", on_click=authenticate)
