796049013034fe87423a4fa99602bdfe6442c80f
lungsang-bod <lungsang@esukhia.org>
2022-10-06 12:47:34 +0530

    redirect_uri

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pydrive.auth import GoogleAuth
 
 def authenticate():
     gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()
+    gauth.redirect_uri = 'http://13.232.4.181:8503'
 
 
 st.button("click here", on_click=authenticate)
